2fc2384d98873117c1640d49ea333d5ee78f97a2
Aliyu <122824528+RealKingTino@users.noreply.github.com>
2024-01-11 00:38:09 +0100

    run.py

--- run.py
+++ run.py
@@ -3,4 +3,6 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port, debug=True)
